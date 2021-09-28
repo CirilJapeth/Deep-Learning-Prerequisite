dcad6c6d5c35c841ca28dda967075b53f88a0c08
Utkrist Karky <63005844+utkrist-karky@users.noreply.github.com>
2021-09-28 21:19:04 +0200

    images for ml

--- 5.Machine_Learning/7. Pre-trained model/VGGNet19.py
+++ 5.Machine_Learning/7. Pre-trained model/VGGNet19.py
@@ -41,8 +41,59 @@ def object_recognition(img_path):
   #display prediction text over the image
   cv2.putText(disp_img, actual_prediction[0][0][1], (20,20), cv2.FONT_HERSHEY_TRIPLEX , 0.8, (0,0,0))
   #show the image
-  cv2.imshow('mat',disp_img)
+  cv2.imshow('result',disp_img)
   cv2.waitKey(0)
 
-img_path = './cat2.jpg'
+
+# img_path = './images/1.png'
+# object_recognition(img_path)
+
+
+# img_path = './images/car.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/cat.png'
+# object_recognition(img_path)
+
+# img_path = './images/cat2.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test1.jpg'
+# object_recognition(img_path)
+
+img_path = './images/test2.jpg'
 object_recognition(img_path)
+
+# img_path = './images/test3.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test4.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test5.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test6.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test7.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test8.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test9.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test10.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test11.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test12.jpg'
+# object_recognition(img_path)
+
+# img_path = './images/test12.jpg'
+# object_recognition(img_path)
+
